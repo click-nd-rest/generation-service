a3ad80149685ce1f248a020dff1e1bbba7a2081a
nikitatorop <torop.nikita1@gmail.com>
2022-12-03 16:28:03 +0300

    Add endpoints security

--- generation-service/src/main/java/com/github/click/nd/rest/generation/service/config/WebSecurityConfig.java
+++ generation-service/src/main/java/com/github/click/nd/rest/generation/service/config/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.github.click.nd.rest.generation.service.config;
 
+import com.github.click.nd.rest.generation.service.security.AuthenticationConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public WebSecurityCustomizer customizer() {
-        return web -> web.ignoring().antMatchers("/**");
+    public SecurityFilterChain customizer(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/v1/api-definition").hasRole("user")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new AuthenticationConfigurer());
+        return httpSecurity.build();
     }
 }
