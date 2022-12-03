0cdfef8b7afce54090e0b0dc100d6d4bee84535b
nikitatorop <torop.nikita1@gmail.com>
2022-12-03 17:20:32 +0300

    Refactor code after 2nd review

--- generation-service/src/main/java/com/github/click/nd/rest/generation/service/security/FilterConfigurer.java
+++ generation-service/src/main/java/com/github/click/nd/rest/generation/service/security/FilterConfigurer.java
@@ -1,5 +1,6 @@
 package com.github.click.nd.rest.generation.service.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.click.nd.rest.generation.service.exception.handlers.FilterExceptionHandler;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +14,7 @@ public class FilterConfigurer extends SecurityConfigurerAdapter<DefaultSecurityF
         UserIdAuthenticationFilter filter = new UserIdAuthenticationFilter();
         httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
-        FilterExceptionHandler exceptionHandlerFilter = new FilterExceptionHandler();
+        FilterExceptionHandler exceptionHandlerFilter = new FilterExceptionHandler(new ObjectMapper());
         exceptionHandlerFilter.afterPropertiesSet();
         httpSecurity.addFilterBefore(exceptionHandlerFilter, UserIdAuthenticationFilter.class);
     }
