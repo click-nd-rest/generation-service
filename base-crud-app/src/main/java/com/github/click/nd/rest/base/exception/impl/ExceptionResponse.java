f9e7e9f97c81222fd9dffa70f0847f04856853f6
nikitatorop <torop.nikita1@gmail.com>
2022-12-04 17:41:50 +0300

    Delete base-crud-app

--- base-crud-app/src/main/java/com/github/click/nd/rest/base/exception/impl/ExceptionResponse.java
+++
@@ -1,7 +0,0 @@
-package com.github.click.nd.rest.base.exception.impl;
-
-import org.springframework.http.HttpStatus;
-
-public record ExceptionResponse(RuntimeException exception, HttpStatus status) {
-
-}
